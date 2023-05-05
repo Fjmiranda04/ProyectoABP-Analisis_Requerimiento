2f772233f77bea7b76cf3c5d68e1c2e7f46b6c36
Franklin Miranda <franklin_miranda04@outlook.com>
2023-05-05 10:55:20 -0500

    Se agrego al repositorio la Carpeta Controlador. Esta carpeta almacena los controladores de funcionalidad del proyecto.

---
+++ src/Controlador/Controlador_LoginBD.java
@@ -0,0 +1,64 @@
+package Controlador;
+
+import Modelo.LoginBD;
+import Modelo.ModeloLogin;
+import Vista.EntradaAdministrador;
+import Vista.LoginUI;
+import Vista.RegistroUsuario_UI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+
+public class Controlador_LoginBD implements ActionListener {
+
+    Modelo.ModeloLogin lg = new Modelo.ModeloLogin();
+    LoginBD login = new LoginBD();
+
+    private LoginUI view;
+    private LoginBD modelo = new LoginBD();
+
+    public void Eventos() {
+        view.btnacceder.addActionListener(this);
+        view.btncrearUsuario.addActionListener(this);
+    }
+
+    public Controlador_LoginBD(LoginUI view) {
+        this.view = view;
+        Eventos();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        Object evt = e.getSource();
+
+        if (evt.equals(view.btnacceder)) {
+
+            String usuario = view.txtusuario.getText();
+            String password = String.valueOf(view.txtpassword.getPassword());
+
+            if (!"".equals(usuario) || !"".equals(password)) {
+
+                lg = login.log(usuario, password);
+
+                if (lg.getUsuario() != null && lg.getPassword() != null) {
+                    EntradaAdministrador sistemas = new EntradaAdministrador();
+                    sistemas.setVisible(true);
+                    view.dispose();
+
+                } else {
+                    JOptionPane.showMessageDialog(null, "USUARIO O LA CONTRASENA INCORRECTAS");
+                }
+
+            }
+        } else if (evt.equals(view.btncrearUsuario)) {
+
+            RegistroUsuario_UI crearCuenta = new RegistroUsuario_UI();
+            crearCuenta.setVisible(true);
+            view.dispose();
+
+        }
+    }
+}
---
+++ src/Controlador/Controlador_NuevoUsuario.java
@@ -0,0 +1,63 @@
+package Controlador;
+
+import Modelo.ModeloRegistro;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import Modelo.RegistroUsuarioBD;
+import Vista.LoginUI;
+import Vista.RegistroUsuario_UI;
+import javax.swing.JOptionPane;
+
+public class Controlador_NuevoUsuario implements ActionListener {
+
+    ModeloRegistro registro = new ModeloRegistro();
+    RegistroUsuarioBD nuevoUsuario = new RegistroUsuarioBD();
+
+    private RegistroUsuario_UI view;
+    private RegistroUsuarioBD modelo = new RegistroUsuarioBD();
+
+    public void Eventos() {
+
+        view.btnCrearCuenta.addActionListener(this);
+
+    }
+
+    public Controlador_NuevoUsuario(RegistroUsuario_UI view) {
+        this.view = view;
+        Eventos();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object evt = e.getSource();
+
+        if (evt.equals(view.btnCrearCuenta)) {
+
+            if (!"".equals(view.txtCrearCuenta_Nombre.getText()) || !"".equals(view.txtCrearCuenta_Apellido.getText()) || !"".equals(view.txtCrearCuenta_Dni.getText())
+                    || !"".equals(view.txtCrearCuenta_Edad.getText()) || !"".equals(view.txtCrearCuenta_Telefono.getText()) || !"".equals(view.txtCrearCuenta_Usuario.getText())
+                    || !"".equals(view.txtCrearCuenta_Password.getText())) {
+
+                registro.setNombre(view.txtCrearCuenta_Nombre.getText());
+                registro.setApellido(view.txtCrearCuenta_Apellido.getText());
+                registro.setDni(Integer.parseInt(view.txtCrearCuenta_Dni.getText()));
+                registro.setEdad(Integer.parseInt(view.txtCrearCuenta_Edad.getText()));
+                registro.setTelefono(view.txtCrearCuenta_Telefono.getText());
+                registro.setUsuario(view.txtCrearCuenta_Usuario.getText());
+                registro.setPassword(view.txtCrearCuenta_Password.getText());
+
+                nuevoUsuario.registrarUsuario(registro);
+
+                JOptionPane.showMessageDialog(null, "CLIENTE REGISTRADO SASTICFACTORIAMENTE");
+                LoginUI login = new LoginUI();
+                login.setVisible(true);
+                view.dispose();
+
+            } else {
+                JOptionPane.showMessageDialog(null, "LOS CAMPOS ESTAN VACIOS, VUELVALO A INTENTAR");
+            }
+
+        }
+
+    }
+
+}
