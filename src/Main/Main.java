b33b3036881a89d25ae6f6afa1a81226c46a78bb
Franklin Miranda <franklin_miranda04@outlook.com>
2023-05-05 10:59:20 -0500

    Se agrego al repositorio la Carpeta Main. Esta carpeta es la que usamos para ejecutar desde 0 el proyecto.

---
+++ src/Main/Main.java
@@ -0,0 +1,21 @@
+
+
+package Main;
+
+import Vista.LoginUI;
+
+/**
+ *
+ * @author Franklyn Miranda
+ */
+public class Main {
+
+   
+    public static void main(String[] args) {
+            
+            LoginUI vista = new LoginUI();
+            vista.setVisible(true);
+
+    }
+
+}
