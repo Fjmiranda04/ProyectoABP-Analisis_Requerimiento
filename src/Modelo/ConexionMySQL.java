37f39b71585e64161a296bc85126487f08d5942d
Franklin Miranda <franklin_miranda04@outlook.com>
2023-05-06 18:36:18 -0500

    Se le realizo modificacion al Archivo Conexion.java cambiandole el nombre a ConexionMySQL.java. realizando la conexion correspondiente al puerto 3307 el cual es el puerto designado a la base de datos del Proyecto.

--- src/Modelo/ConexionMySQL.java
+++ src/Modelo/ConexionMySQL.java
@@ -1,21 +1,42 @@
 /*BREVE EXPLICACION:
-Esta clase (Conexion) es el puente a la conexion de nuestra BD
-es la clase mas importante si deseamos tener una comunicacion con lo que es 
-nuestra BD*/
+ Esta clase (ConexionMySQL) es el puente a la conexion de nuestra BD
+ es la clase mas importante si deseamos tener una comunicacion con lo que es 
+ nuestra BD*/
 package Modelo;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Conexion {
-
-    Connection con;
+public class ConexionMySQL {
+/*
+   private String database_name = "bd_sistema_parqueo";
+    private String user = "Soporte";
+    private String password = "Fj_Miranda22/";
+    private String url = "jdbc:mysql://localhost:3307/" + database_name;
+    Connection con = null;
 
     public Connection getConnection() {
         try {
-            String myBD = "jdbc:mysql://localhost:3306/sistemaparqueo?serverTimezone=UTC";
-            con = DriverManager.getConnection(myBD, "root", "");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            con = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException e) {
+            System.err.println("ha ocurrido un ClassNotFoundException" + e.getMessage());
+
+        } catch (SQLException e) {
+            System.err.println("ha ocurrido un SQLException" + e.getMessage());
+        }
+        return con;
+
+    }
+    */
+   Connection con;
+
+    public Connection getConnection() {
+        try {
+            String myBD = "jdbc:mysql://localhost:3307/bd_sistema_parqueo?serverTimezone=UTC";
+            con = DriverManager.getConnection(myBD, "Soporte", "Fj_Miranda22/");
             return con;
         } catch (SQLException e) {
             System.out.println(e.toString());
