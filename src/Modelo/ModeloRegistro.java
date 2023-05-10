4bf627ff506af855a50fdbf0dae802ee12f23e7a
Franklin Miranda <franklin_miranda04@outlook.com>
2023-05-09 20:47:10 -0500

    Se realizaron modificaiones al archivo ModeloRegistro.java. Los cuales fueron incorporacion de nuevas variables asi como la actualizacion de su constructor y sus metodos get y set.

--- src/Modelo/ModeloRegistro.java
+++ src/Modelo/ModeloRegistro.java
@@ -1,9 +1,10 @@
 package Modelo;
 
-public class ModeloRegistro extends ModeloLogin {
+public class ModeloRegistro extends ModeloLoginUsuario {
 
     private String nombre;
     private String apellido;
+    private int tipoDoc;
     private int dni;
     private int edad;
     private String telefono;
@@ -11,10 +12,11 @@ public class ModeloRegistro extends ModeloLogin {
     public ModeloRegistro() {
     }
 
-    public ModeloRegistro(String nombre, String apellido, int dni, int edad, String telefono, int id, String usuario, String password) {
+    public ModeloRegistro(String nombre, String apellido, int tipoDoc, int dni, int edad, String telefono, int id, String usuario, String password) {
         super(id, usuario, password);
         this.nombre = nombre;
         this.apellido = apellido;
+        this.tipoDoc = tipoDoc;
         this.dni = dni;
         this.edad = edad;
         this.telefono = telefono;
@@ -34,6 +36,14 @@ public class ModeloRegistro extends ModeloLogin {
 
     public void setApellido(String apellido) {
         this.apellido = apellido;
+    }
+
+    public int getTipoDoc() {
+        return tipoDoc;
+    }
+
+    public void setTipoDoc(int tipoDoc) {
+        this.tipoDoc = tipoDoc;
     }
 
     public int getDni() {
